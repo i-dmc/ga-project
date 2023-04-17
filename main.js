74d9f7c95fba9eb462c09e2bf9afb718b1b24219
i-dmc <iva.dujmovic@hotmail.com>
2023-04-17 23:57:44 +1000

    commit

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-function
 
 const burgerMenu = document.querySelector('.burger-menu');
 const navLinks = document.querySelector('nav ul');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 
 burgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    dropdownMenu.classList.toggle('show-menu'); // toggle the show-menu class
 });
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-
-hamburgerMenu.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('show-menu');
+document.addEventListener('click', (event) => {
+    const isClickInsideNav = event.target.closest('nav') !== null;
+    const isClickInsideDropdown = event.target.closest('.dropdown-menu') !== null;
+    if (!isClickInsideNav && !isClickInsideDropdown) {
+        navLinks.classList.remove('active');
+        dropdownMenu.classList.remove('show-menu');
+    }
 });
